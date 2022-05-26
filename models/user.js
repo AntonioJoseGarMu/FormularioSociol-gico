70735a9d497fd598a68058521874683ee59d4f05
AntonioJoseGarMu <aj.garmu@gmail.com>
2022-05-26 16:56:29 +0200

    Estructura Ricardo

--- models/user.js
+++ models/user.js
@@ -3,9 +3,8 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const bcrypt = require('bcryptjs');
-const saltRounds = 10;
 
-const UserSchema = new Schema( {
+const UserSchema = new Schema({
     email: { type: String, unique: true, lowercase: true },
     displayName: String,
     password: String,
@@ -13,20 +12,20 @@ const UserSchema = new Schema( {
     lastLogin: Date
 })
 
-UserSchema.pre('save', function(next) {
+UserSchema.pre('save', function (next) {
     let user = this
     if (!user.isModified('password')) return next()
 
-        bcrypt.hash(user.password, saltRounds, (err, hash) => {
-            if(err) return next(err);
+    bcrypt.genSalt(10, (err, salt) => {
+        if (err) return next(err)
 
-            user.password = hash;
+        bcrypt.hash(user.password, salt, null, (err, hash) => {
+            if (err) return next(err)
+
+            user.password = hash
             next()
         })
-        bcrypt.compare(user.password, hash, (err, result) => {
-            if(err) return err;
-            result == true;
-        })
+    })
 })
 
 module.exports = mongoose.model('User', UserSchema);
