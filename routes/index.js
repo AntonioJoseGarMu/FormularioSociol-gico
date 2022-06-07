f13b4c35e8aa8dd869016c6f1247305ab386c422
AntonioJoseGarMu <aj.garmu@gmail.com>
2022-06-07 19:43:48 +0200

    Clear

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,9 @@ const userCtrl = require('../controllers/user');
 const auth = require('../middlewares/auth');
 const api = express.Router();
 
-// api.get('/product', ProductCtrl.getProducts);
 api.get('/encuesta/:encuestasId', auth, EncuestaCtrl.getEncuestas);
 api.post('/encuesta', auth, EncuestaCtrl.saveEncuesta);
 api.get('/allEncuestas', auth, EncuestaCtrl.getAllEncuestas);
-// api.put('/product/:productId', ProductCtrl.updateProduct);
-// api.delete('/product/:productId', ProductCtrl.deleteProduct);
 api.post('/signup', userCtrl.signUp);
 api.post('/signin', userCtrl.signIn);
 api.get('/users', auth, userCtrl.getUsers);
